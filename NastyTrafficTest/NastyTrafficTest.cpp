4200bd671275ba41203b1a3e0a4b7311dd2e1424
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2021-09-28 22:06:44 +0300

    add: project init, really empty

---
+++ NastyTrafficTest/NastyTrafficTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NastyTrafficTest
+{
+	TEST_CLASS(NastyTrafficTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
