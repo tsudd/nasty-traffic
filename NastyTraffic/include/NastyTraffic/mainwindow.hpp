0a2f27caa6c80e3731005982c08252dc835e7584
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2021-11-27 00:17:59 +0300

    add: download speed graph add: getting devices add: download speed graph

--- NastyTraffic/include/NastyTraffic/mainwindow.hpp
+++ NastyTraffic/include/NastyTraffic/mainwindow.hpp
@@ -12,15 +12,19 @@
 #include <QTreeWidgetItem>
 #include "packetorchestrator.hpp"
 #include <thread>
-#include <chrono>
 #include <QGraphicsScene>
 #include <QLineSeries>
+#include "qcustomplot.hpp"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QWidget {
+    struct  point {
+        double key;
+        double value;
+    };
     Q_OBJECT
 
 public:
@@ -30,24 +34,30 @@ public:
 public slots:
     void select_device(int index);
     void toggle_process();
+    void download_range_changed();
+    void clear_graphs();
 private:
     std::thread* dumping;
     std::thread* upd;
 
-    QGraphicsScene* downScene;
-//    QLineSeries* seris = new QLineSeries();
+    QTimer* dataTimer;
 
+    QCustomPlot* downPlt;
+    QQueue<double> downPnts;
+    QQueue<double> keys;
 
+    double time_passed = 0;
     int sniffed_packets = 0;
     bool is_reading = false;
     PacketOrchestrator* traffic;
     Ui::MainWindow *ui;
+    void addPoint(const double p);
     void closeEvent(QCloseEvent *event) override;
     void clear();
 
     QTreeWidgetItem* packetModel;
 
-    void show_packets(const float duration, const int amount);
+    void show_packets(const double duration);
     QString convert_bytes_to_speed(const long long bytes_amount, const float time);
 };
 
--- NastyTraffic/src/mainwindow.cpp
+++ NastyTraffic/src/mainwindow.cpp
@@ -3,6 +3,7 @@
 //
 
 // You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+#define POINTS_AMOUNT 20
 
 #include "mainwindow.hpp"
 #include "ui_MainWindow.h"
@@ -10,21 +11,61 @@
 #include <QMessageBox>
 #include <QCloseEvent>
 #include <cmath>
+#include <chrono>
 
 MainWindow::MainWindow(QWidget *parent) :
         QWidget(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
-//    auto sniffer = new NastyTraffic();
     traffic = new PacketOrchestrator();
+    dataTimer = new QTimer();
     ui->devicesBox->addItems(QStringList(traffic->get_devices_names()));
 
-    downScene = new QGraphicsScene();
-//    QLineSeries* series = new QLineSeries();
-//    series->add(0, 6);
-//    series->add(2, 4);
-//    ui->downloadGraph->setScene(downloadPlt);
+    downPlt = new QCustomPlot();
+    ui->graphGrid->addWidget(downPlt, 0, 0, 1, 1);
 
-//    QObject::connect(&ui->devicesBox, &QComboBox::currentIndexChanged, )
+
+//    downPlt->setInteraction(QCP::iRangeZoom, true);
+    downPlt->setInteraction(QCP::iRangeDrag, true);
+    downPlt->axisRect()->setRangeDrag(Qt::Horizontal);
+//    downPlt->axisRect()->setRangeZoom(Qt::Horizontal);
+    downPlt->xAxis->setSubTicks(true);
+
+    downPlt->xAxis2->setVisible(true);
+    downPlt->yAxis2->setVisible(true);
+    downPlt->xAxis2->setTicks(false);
+    downPlt->yAxis2->setTicks(false);
+    downPlt->xAxis2->setTickLabels(false);
+    downPlt->yAxis2->setTickLabels(false);
+
+    downPlt->yAxis->setTickLabelColor(QColor(Qt::red));
+    downPlt->legend->setVisible(true);
+    downPlt->yAxis->setRange(0, 13000);
+
+    downPlt->axisRect()->insetLayout()->setInsetAlignment(0, Qt::AlignLeft|Qt::AlignTop);
+
+//    downGraph = new QCPGraph(downPlt->xAxis, downPlt->yAxis);
+//    downPlt->addPlttable
+    downPlt->addGraph();
+    downPlt->graph(0)->setName("Download, B/s");
+    downPlt->graph(0)->setPen(QPen(QColor(40, 110, 255)));
+    downPlt->graph(0)->setAntialiased(false);
+    downPlt->graph(0)->setLineStyle(QCPGraph::lsLine);
+
+    QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
+    timeTicker->setTimeFormat("%m:%s");
+    downPlt->yAxis->setRange(0, 20000);
+    downPlt->xAxis->setTicker(timeTicker);
+    downPlt->axisRect()->setupFullAxesBox();
+
+    connect(downPlt->xAxis, SIGNAL(rangeChanged(QCPRange)),
+            downPlt->xAxis2, SLOT(setRange(QCPRange)));
+
+    connect(downPlt->yAxis, SIGNAL(rangeChanged(QCPRange)),
+            downPlt->yAxis2, SLOT(setRange(QCPRange)));
+
+    connect(dataTimer, SIGNAL(timeout()), this, SLOT(download_range_changed()));
+//    downPlt->graph(0)->setData(time, downPoints);
+
     connect((ui->devicesBox), SIGNAL(currentIndexChanged(int)), this, SLOT(select_device(int)));
     connect(ui->processButton, SIGNAL(clicked()), this, SLOT(toggle_process()));
     packetModel = ui->treeView->invisibleRootItem();
@@ -45,30 +86,28 @@ void MainWindow::toggle_process() {
 
         dumping->detach();
         upd->detach();
-
+        dataTimer->stop();
         delete dumping;
         delete upd;
     } else {
         clear();
         is_reading = true;
+        dataTimer->start(0);
         ui->processButton->setText("Stop");
         ui->statusIndicator->setText("Sniffing...");
         sniffed_packets = 0;
-
-        auto start = std::chrono::high_resolution_clock::now();
+        clear_graphs();
 
         dumping = new std::thread(&PacketOrchestrator::read_device_live, traffic, std::ref(is_reading));
 
         upd = new std::thread([&]() {
-            while(is_reading) {
-//                ui->packagesIndicator->setText(QString::number(packet_len));
-            auto tm = std::chrono::high_resolution_clock::now();
-            int packets = traffic->get_sniffed_packets_amount();
-            std::chrono::duration<float> duration = tm - start;
-            show_packets(duration.count(), packets);
+            auto start = std::chrono::steady_clock::now();
+            do {
+            auto tm = std::chrono::steady_clock::now();
+            show_packets(std::chrono::duration_cast<std::chrono::seconds>(tm-start).count());
             std::this_thread::sleep_for(std::chrono::seconds(1));
             start = tm;
-            }
+            } while(is_reading);
         });
     }
 }
@@ -82,6 +121,7 @@ MainWindow::~MainWindow() {
 //    delete dumping;
     delete traffic;
     delete ui;
+    delete dataTimer;
 }
 
 void MainWindow::clear() {
@@ -89,8 +129,11 @@ void MainWindow::clear() {
     traffic->clear_packets();
 }
 
-void MainWindow::show_packets(const float duration, const int amount) {
-    if (amount == 0) {
+void MainWindow::show_packets(const double duration) {
+    time_passed += duration;
+    int amount = traffic->get_sniffed_packets_amount();
+    if (amount - sniffed_packets == 0) {
+        addPoint(0);
         return;
     }
     int packets = 0;
@@ -100,12 +143,13 @@ void MainWindow::show_packets(const float duration, const int amount) {
         auto pckt = traffic->get_packet(i);
         packetModel->insertChild(sniffed_packets, traffic->get_packet_item(pckt));
         if (traffic->is_packet_received(pckt)) {
-            downloaded += pckt->size_payload;
+            downloaded += pckt->size_payload / 8;
         } else {
             uploaded += pckt->size_payload;
         }
         packets++;
     }
+    addPoint(downloaded / 1024.0 / duration);
     ui->packagesIndicator->setText(QString::number(packets));
     ui->downloadIndicator->setText(convert_bytes_to_speed(downloaded, duration));
     ui->uploadIndicator->setText(convert_bytes_to_speed(uploaded, duration));
@@ -117,7 +161,7 @@ QString MainWindow::convert_bytes_to_speed(const long long bytes_amount, const f
         return "0";
     }
     double speed;
-    speed = (bytes_amount / 8.0) / time;
+    speed = bytes_amount / time;
 
     int division_times = 0;
     do {
@@ -151,6 +195,33 @@ void MainWindow::closeEvent(QCloseEvent *event) {
 //        if (upd)
 //            upd->detach();
         event->accept();
+    }
+}
+
+void MainWindow::addPoint(const double p) {
+    downPnts.dequeue();
+    keys.dequeue();
+    downPnts.enqueue(p);
+    keys.enqueue(time_passed);
+}
+
+void MainWindow::download_range_changed() {
+    if (downPnts.isEmpty() || keys.isEmpty()) {
+        return;
+    }
+    downPlt->graph(0)->setData(keys.toVector(), downPnts.toVector());
+//    downPlt->graph(0)->rescaleValueAxis();
+    downPlt->xAxis->setRange(time_passed, 8, Qt::AlignRight);
+    downPlt->replot();
+}
+
+void MainWindow::clear_graphs() {
+    keys.clear();
+    downPnts.clear();
+
+    for (int i = 0; i < POINTS_AMOUNT; i++) {
+        downPnts.enqueue(0);
+        keys.enqueue(0);
     }
 }
 
